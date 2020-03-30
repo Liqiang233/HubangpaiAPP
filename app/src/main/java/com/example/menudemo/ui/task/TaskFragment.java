ccdcacfd9b5093b0dad8d246089e2d00cbff9e8b
unknown <you@example.com>
2020-03-30 21:37:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/menudemo/ui/task/TaskFragment.java
@@ -0,0 +1,35 @@
+package com.example.menudemo.ui.task;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.menudemo.R;
+
+public class TaskFragment extends Fragment {
+
+    private TaskViewModel dashboardViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        dashboardViewModel =
+                ViewModelProviders.of(this).get(TaskViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_task, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        dashboardViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
