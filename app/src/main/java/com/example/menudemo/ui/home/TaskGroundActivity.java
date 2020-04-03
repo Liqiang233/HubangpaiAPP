717db958853c241d06e104a2db2d3e7964281cd4
TendaG <81>
2020-04-03 11:05:44 +0800

    gtd2

--- app/src/main/java/com/example/menudemo/ui/home/TaskGroundActivity.java
+++ app/src/main/java/com/example/menudemo/ui/home/TaskGroundActivity.java
@@ -13,5 +13,6 @@ public class TaskGroundActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_ground);
+        finish();
     }
 }
