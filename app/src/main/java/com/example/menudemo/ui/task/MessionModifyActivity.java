8f596dcfd63f968669425b137f3a2ab0833c6778
TendaG <81>
2020-05-15 12:43:20 +0800

    新增时间选择器

--- app/src/main/java/com/example/menudemo/ui/task/MessionModifyActivity.java
+++ app/src/main/java/com/example/menudemo/ui/task/MessionModifyActivity.java
@@ -15,10 +15,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.menudemo.MainActivity;
 import com.example.menudemo.R;
+import com.example.menudemo.ui.datepicker.CustomDatePicker;
+import com.example.menudemo.ui.datepicker.DateFormatUtils;
 import com.example.menudemo.ui.utills.HttpUtillConnection;
 
 import org.json.JSONException;
@@ -26,6 +29,7 @@ import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -35,8 +39,8 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
 
 
     //String url = HttpUtillConnection.BASE_URL_ModifyTask;  @Tenda
-    String url = HttpUtillConnection.Ya_URL+"ModifyTask";   //@Ya
-    public String CompleUrl = HttpUtillConnection.Ya_URL+"CompleteTask";
+    String url = HttpUtillConnection.gtd_URL+"ModifyTask";   //@gtd
+    public String CompleUrl = HttpUtillConnection.gtd_URL+"CompleteTask";
     // String url=                  //@liqiang
 
 
@@ -45,7 +49,7 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
     private EditText mname=null;
     private EditText maddress=null;
     private EditText mdetail=null;
-    private EditText mdeadline=null;
+    //private EditText mdeadline=null;
     private EditText mpay=null;
     private Button mcommit=null;
     private Context context;
@@ -55,9 +59,12 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
     private String Address;
     private String Price;
     private String Details;
+    private String mdeadline=null;
     private String id;
     private Button mcomple;
     private String status;
+    private TextView mTvSelectedTime;
+    private CustomDatePicker mTimerPicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -65,12 +72,13 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
         context = getApplicationContext();
         mclass=findViewById(R.id.md_messionclass);
         mname=findViewById(R.id.md_messionname);
-        mdeadline=findViewById(R.id.md_messiondeadline);
+       // mdeadline=findViewById(R.id.md_messiondeadline);
         maddress=findViewById(R.id.md_messionaddress);
         mcommit=findViewById(R.id.md_modify);
         mpay=findViewById(R.id.md_messionpay);
         mdetail=findViewById(R.id.md_messiondetails);
         mcomple=findViewById(R.id.md_comple);
+        mTvSelectedTime = findViewById(R.id.tv2_selected_time);
 
         //界面间传递数据
         Intent intent = getIntent();
@@ -95,9 +103,17 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
         maddress.setText(intent.getStringExtra("messionaddress"));
         mdetail.setText(intent.getStringExtra("messiondetails"));
         mpay.setText(intent.getStringExtra("messionprice"));
-        mdeadline.setText(intent.getStringExtra("messiondeadline"));
+        mdeadline=intent.getStringExtra("messiondeadline");
         status=intent.getStringExtra("messionstatus");
         id = intent.getStringExtra("messionid");
+        initTimerPicker();
+        mTvSelectedTime.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                mTimerPicker.show(mTvSelectedTime.getText().toString());
+            }
+        });
         if (status.equals("完结申请中")) {
             mpay.setEnabled(false);
             mcommit.setEnabled(false);
@@ -106,7 +122,7 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
         } else if(status.equals("已完结")){
             mclass.setEnabled(false);
             mname.setEnabled(false);
-            mdeadline.setEnabled(false);
+            mTvSelectedTime.setEnabled(false);
             maddress.setEnabled(false);
             mcommit.setEnabled(false);
             mpay.setEnabled(false);
@@ -128,7 +144,7 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
             public void onClick(View view) {
                 MessionName = mname.getText().toString();
                 MessionType = mclass.getSelectedItem().toString();
-                Deadline = mdeadline.getText().toString();
+                Deadline = mTvSelectedTime.getText().toString();
                 Address = maddress.getText().toString();
                 Price = mpay.getText().toString();
                 Details = mdetail.getText().toString();
@@ -139,7 +155,7 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
 
 
                         Map<String, String> params = new HashMap<String, String>();
-                        params.put("MesionID", id);
+                        params.put("MessionID", id);
                         params.put("MessionName", MessionName);
                         params.put("MessionType", MessionType);
                         params.put("Price", Price);
@@ -272,5 +288,35 @@ public class MessionModifyActivity extends AppCompatActivity implements AdapterV
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    private void initTimerPicker() {
+        //获取当前日期
+        Date date = new Date();
+        //创建Calendar实例
+        Calendar cal1 = Calendar.getInstance();
+
+        cal1.setTime(date);   //设置截至时间
+
+        //增加一个月的方法：
+        cal1.add(Calendar.MONTH, 1);
+        //将时间格式化成yyyy-MM-dd HH:mm:ss的格式
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        String endTime = format.format(cal1.getTime());
+
+        String beginTime = mdeadline;
+        mTvSelectedTime.setText(beginTime);
+
+        // 通过日期字符串初始化日期，格式请用：yyyy-MM-dd HH:mm
+        mTimerPicker = new CustomDatePicker(MessionModifyActivity.this, timestamp -> mTvSelectedTime.setText(DateFormatUtils.long2Str(timestamp, true)), beginTime, endTime);
+        // 允许点击屏幕或物理返回键关闭
+        mTimerPicker.setCancelable(true);
+        // 显示时和分
+        mTimerPicker.setCanShowPreciseTime(true);
+        // 允许循环滚动
+        mTimerPicker.setScrollLoop(true);
+        // 允许滚动动画
+        mTimerPicker.setCanShowAnim(true);
     }
 }
--- app/src/main/java/com/example/menudemo/ui/task/PublishActivity.java
+++ app/src/main/java/com/example/menudemo/ui/task/PublishActivity.java
@@ -21,6 +21,9 @@ import android.widget.Toast;
 
 import com.example.menudemo.MainActivity;
 import com.example.menudemo.R;
+import com.example.menudemo.ui.datepicker.CustomDatePicker;
+import com.example.menudemo.ui.datepicker.DateFormatUtils;
+import com.example.menudemo.ui.datepicker.PickerView;
 import com.example.menudemo.ui.home.LoginActivity;
 import com.example.menudemo.ui.utills.HttpUtillConnection;
 
@@ -30,23 +33,24 @@ import org.json.JSONObject;
 import java.lang.reflect.Array;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
 
-public class PublishActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+public class PublishActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,View.OnClickListener{
 
 
 
     //  String url = HttpUtillConnection.BASE_URL_Task_Ya;
-    String url = HttpUtillConnection.Ya_URL+"PublishTask";
+    String url = HttpUtillConnection.gtd_URL+"PublishTask";
 
     private Spinner mclass=null;
     private EditText mname=null;
     private EditText maddress=null;
     private EditText mdetail=null;
-    private EditText mdeadline=null;
+    //private TextView mdeadline=null;
     private EditText mpay=null;
     private Button mcommit=null;
     private SharedPreferences sp;
@@ -60,6 +64,8 @@ public class PublishActivity extends AppCompatActivity implements AdapterView.On
     private String LaunchTime;
     private String Details;
     private String Status;
+    private TextView mTvSelectedTime;
+    private CustomDatePicker mTimerPicker;
     private static  final String USERINFO = "userInfo";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,25 +78,26 @@ public class PublishActivity extends AppCompatActivity implements AdapterView.On
         sp = getSharedPreferences(USERINFO , MODE_PRIVATE);
         mclass=findViewById(R.id.sp_messionclass);
         mname=findViewById(R.id.tv_messionname);
-        mdeadline=findViewById(R.id.tv_messiondeadline);
+        findViewById(R.id.ll_time).setOnClickListener(this);
+        mTvSelectedTime = findViewById(R.id.tv_selected_time);
+        initTimerPicker();
         maddress=findViewById(R.id.tv_messionaddress);
         mpay=findViewById(R.id.tv_messionpay);
         mcommit=findViewById(R.id.bt_publish);
         mdetail=findViewById(R.id.tv_messiondetails);
-
         mcommit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MessionName = mname.getText().toString();
                 MessionType = mclass.getSelectedItem().toString();
-                Deadline = mdeadline.getText().toString();
+                Deadline = mTvSelectedTime.getText().toString();
                 Address = maddress.getText().toString();
                 Price = mpay.getText().toString();
                 Initiator = sp.getString("id", null);
                 Details = mdetail.getText().toString();
                 Status = "待接受";
                 if(mname.getText().toString().trim().equals("")||mclass.getSelectedItem().toString().trim().equals("")||
-                        mdeadline.getText().toString().trim().equals("")||maddress.getText().toString().trim().equals("")||
+                        mTvSelectedTime.getText().toString().trim().equals("")||maddress.getText().toString().trim().equals("")||
                         mpay.getText().toString().trim().equals("")){
                     Toast.makeText(PublishActivity.this,"请填写完整信息",Toast.LENGTH_SHORT).show();
                 }
@@ -193,5 +200,46 @@ public class PublishActivity extends AppCompatActivity implements AdapterView.On
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_time:
+                // 日期格式为yyyy-MM-dd HH:mm
+                mTimerPicker.show(mTvSelectedTime.getText().toString());
+                break;
+        }
+    }
+    private void initTimerPicker() {
+        //String beginTime = DateFormatUtils.long2Str(System.currentTimeMillis(), true);
+        //String endTime = DateFormatUtils.long2Str(System.currentTimeMillis(), true);
+        //获取当前日期
+        Date date = new Date();
+        //创建Calendar实例
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(date);   //设置截至时间
+        cal2.setTime(date);
+        //增加一个月的方法：
+        cal1.add(Calendar.MONTH, 1);
+        cal2.add(Calendar.DAY_OF_MONTH, 1);
+        //将时间格式化成yyyy-MM-dd HH:mm:ss的格式
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+        String beginTime =format.format(cal2.getTime());
+        String endTime = format.format(cal1.getTime());
+        mTvSelectedTime.setText(beginTime);
+
+        // 通过日期字符串初始化日期，格式请用：yyyy-MM-dd HH:mm
+        mTimerPicker = new CustomDatePicker(PublishActivity.this, timestamp -> mTvSelectedTime.setText(DateFormatUtils.long2Str(timestamp, true)), beginTime, endTime);
+        // 允许点击屏幕或物理返回键关闭
+        mTimerPicker.setCancelable(true);
+        // 显示时和分
+        mTimerPicker.setCanShowPreciseTime(true);
+        // 允许循环滚动
+        mTimerPicker.setScrollLoop(true);
+        // 允许滚动动画
+        mTimerPicker.setCanShowAnim(true);
     }
 }
